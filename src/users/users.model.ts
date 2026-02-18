9cd73a92c6b30c43d6569b22d4affbc68385cfe8
tim <timu.ryanst@gmail.com>
2026-02-18 20:19:36 +0300

    chore: add postgres and env files

---
+++ src/users/users.model.ts
@@ -0,0 +1,29 @@
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
+
+interface UserCreationAttrs {
+  email: string;
+  password: string;
+}
+
+@Table({ tableName: 'users' })
+export class User extends Model<User, UserCreationAttrs> {
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  declare id: number;
+
+  @Column({ type: DataType.STRING, unique: true, allowNull: false })
+  declare email: string;
+
+  @Column({ type: DataType.STRING, allowNull: false })
+  declare password: string;
+
+  @Column({ type: DataType.BOOLEAN, defaultValue: false })
+  declare banned: boolean;
+
+  @Column({ type: DataType.STRING, allowNull: true })
+  declare banReason: string;
+}
